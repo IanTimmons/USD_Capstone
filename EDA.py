aef955c19d824ba18a13f1ad13635e746bc2f635
Rob Salamon <rob.a.salamon@gmail.com>
2023-03-19 13:20:48 -0700

    Beginning EDA

--- EDA.py
+++ EDA.py
@@ -1,3 +1,15 @@
+"""
+Author: Rob Salamon
+Purpose: 
+
+Description:
+
+
+Inputs:
+data_dir: Folder location of dataset on local machine
+
+"""
+
 import mediapipe as mp
 
 import tensorflow as tf
@@ -13,3 +25,12 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def set_random(seed=43):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+set_random()
+
+data_dir = "C:\Users\robas\Documents\Code\USD_Capstone\data"
